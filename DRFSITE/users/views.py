81f45f3be90fd1eea4d5c64f9f1e847aef7a9dd1
Altst <94057001+Altst@users.noreply.github.com>
2024-04-15 20:58:26 +0300

    Initial commit

---
+++ DRFSITE/users/views.py
@@ -0,0 +1,34 @@
+from rest_framework import generics, status
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.response import Response
+
+
+# Create your views here.
+
+class UserAPILogIn(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            if user.password == password:
+                return Response({'message': 'Вход выполнен успешно'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Неверные учетные данные'}, status=status.HTTP_401_UNAUTHORIZED)
+        except User.DoesNotExist:
+            return Response({'error': 'Пользователь не найден'}, status=status.HTTP_404_NOT_FOUND)
+
+class UserAPISignUp(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        # password = self.request.data.get('password', '')
+        # if len(password) < 8:
+        #     return Response({'error': 'Пароль должен содержать не менее 8 символов'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
